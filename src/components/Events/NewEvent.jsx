b39d2001fa704099c5bf13ea8b656f4548145c48
Hameedat Oyewopo <hameedattimileyinoyewopo@gmail.com>
2024-12-26 17:45:01 +0100

    first commit

--- src/components/Events/NewEvent.jsx
+++ src/components/Events/NewEvent.jsx
@@ -4,28 +4,35 @@ import { createNewEvent } from '../../util/http.js';
 
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
 
 export default function NewEvent() {
-  const{mutate} = useMutation({
+  const navigate = useNavigate();
+
+  const{mutate, isPending, isError, error} = useMutation({
     mutationFn: createNewEvent
   })
 
-  const navigate = useNavigate();
-
-  function handleSubmit(formData) {}
+  function handleSubmit(formData) {
+    mutate({event: formData});
+  }
 
   return (
     <Modal onClose={() => navigate('../')}>
       <EventForm onSubmit={handleSubmit}>
-        <>
-          <Link to="../" className="button-text">
-            Cancel
-          </Link>
-          <button type="submit" className="button">
-            Create
-          </button>
-        </>
+        {isPending && 'Submitting...'}
+        {!isPending && (
+           <>
+           <Link to="../" className="button-text">
+             Cancel
+           </Link>
+           <button type="submit" className="button">
+             Create
+           </button>
+         </>
+        )}
       </EventForm>
+      {isError && <ErrorBlock title='Failed to create event' message={error.info?.message || 'Failed to create event. Please check your inputs and try again later. '}/>}
     </Modal>
   );
 }
